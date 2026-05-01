662abf233a1ae5a32c1018038a62061c78bc9205
rida316 <nafisaakbar83@gmail.com>
2026-05-01 18:53:56 +0500

    Fix typo in lowercase field for User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     trim: true,
-    lowercase: true,
+    lowercase: falsa,
   },
   password: {
     type: String,
