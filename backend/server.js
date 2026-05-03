17acccf9d8837bdfd8de7ccf23f87612329ee77e
Aleeha Akhlaq <165453763+Aleehaa@users.noreply.github.com>
2026-05-03 23:04:52 +0500

    Change server port from 5000 to 5050

--- backend/server.js
+++ backend/server.js
@@ -133,7 +133,7 @@ app.use((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT,'0.0.0.0',() => {
   console.log(`🚀 Server running on port ${PORT}`);
