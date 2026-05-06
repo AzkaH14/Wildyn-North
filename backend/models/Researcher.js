31d9280524fabd55ce48fc49696d9d1a99fde1f8
Aleeha Akhlaq <165453763+Aleehaa@users.noreply.github.com>
2026-05-06 23:59:34 +0500

    email field from Researcher model
    Removed email field from Researcher model.

--- backend/models/Researcher.js
+++ backend/models/Researcher.js
@@ -8,13 +8,7 @@ const researcherSchema = new mongoose.Schema({
     trim: true,
     minlength: 4,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
+ 
   password: {
     type: String,
     required: true,
